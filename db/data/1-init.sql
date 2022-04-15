bb497b7dcf3cf2996d1fd3db78d2a00e0d9e4613
Hybrys <64049236+Hybrys@users.noreply.github.com>
2022-04-15 12:50:47 -0700

    Push hash indexes into db init
    Minor performance improvement on queries.

--- db/data/1-init.sql
+++ db/data/1-init.sql
@@ -4,3 +4,8 @@ CREATE DATABASE "database";
 
 DROP DATABASE IF EXISTS "test";
 CREATE DATABASE "test";
+
+-- In testing, these two hash indexes improved performance
+-- Everything else is either unique or queried via PK
+CREATE INDEX albums_hash_index ON albums(album_title);
+CREATE INDEX tracks_hash_index ON tracks(track_title);
